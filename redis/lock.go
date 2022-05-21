4820404485b3613ca1631ec42e3dad546bb3aede
wandore <916133744@qq.com>
2022-05-21 14:05:20 +0800

    fix

--- redis/lock.go
+++ redis/lock.go
@@ -44,7 +44,7 @@ func (l *Locker) Keep(ctx context.Context, ttl time.Duration) error {
 
 	keepLua := redis.NewScript(`if redis.call("get", KEYS[1]) == ARGV[1] then return redis.call("pexpire", KEYS[1], ARGV[2]) else return 0 end`)
 
-	ok, err := keepLua.Run(ctx, l.redis, []string{l.key}, l.uuid+l.value, release).Result()
+	ok, err := keepLua.Run(ctx, *l.redis, []string{l.key}, l.uuid+l.value, release).Result()
 	if err != nil {
 		return err
 	}
