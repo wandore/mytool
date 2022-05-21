81fc0b747cdf83d4d82f8e046e3e1191c0730937
wandore <916133744@qq.com>
2022-05-21 13:26:08 +0800

    rename go.mod

--- redis/go.mod
+++ redis/go.mod
@@ -1,10 +1,14 @@
-module github.com/mytool/redis
+module github.com/wandore/mytool/redis
 
 go 1.18
 
 require (
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/satori/go.uuid v1.2.0
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis/v8 v8.11.5 // indirect
-	github.com/satori/go.uuid v1.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- redis/redis.go
+++ redis/redis.go
@@ -3,13 +3,8 @@ package redis
 import (
 	"context"
 	"fmt"
-	"time"
-
 	"github.com/go-redis/redis/v8"
-	uuid "github.com/satori/go.uuid"
 )
-
-var LockErr = fmt.Errorf("locking by another node")
 
 type MyRedis struct {
 	redis *redis.Client
@@ -33,64 +28,4 @@ func New(addr, pwd string, db int) (*MyRedis, error) {
 	}
 
 	return ins, nil
-}
-
-func (r *MyRedis) TryLock(ctx context.Context, key, value string, ttl time.Duration) (*Locker, error) {
-	uuid := uuid.NewV4().String()
-
-	ok, err := r.redis.SetNX(ctx, key, uuid+value, ttl).Result()
-	if err != nil {
-		err = fmt.Errorf("try lock error: %v", err)
-		return nil, err
-	}
-	if !ok {
-		return nil, LockErr
-	}
-
-	locker := &Locker{
-		redis: r.redis,
-		key:   key,
-		value: value,
-		uuid:  uuid,
-	}
-	return locker, nil
-}
-
-type Locker struct {
-	redis *redis.Client
-	key   string
-	value string
-	uuid  string
-}
-
-func (l *Locker) Keep(ctx context.Context, ttl time.Duration) error {
-	keepLua := redis.NewScript(`if redis.call("get", KEYS[1]) == ARGV[1] then return redis.call("pexpire", KEYS[1], ARGV[2]) else return 0 end`)
-
-	ok, err := keepLua.Run(ctx, l.redis, []string{l.key, l.uuid + l.value}, ttl).Result()
-	if err != nil {
-		return err
-	}
-	if ok == 1 {
-		return nil
-	}
-
-	return LockErr
-}
-
-func (l *Locker) Unlock(ctx context.Context) error {
-	unlockLua := redis.NewScript(`if redis.call("get", KEYS[1]) == ARGV[1] then return redis.call("del", KEYS[1]) else return 0 end`)
-
-	ok, err := unlockLua.Run(ctx, l.redis, []string{l.key, l.uuid + l.value}).Result()
-	if err == redis.Nil {
-		return LockErr
-	}
-	if err != nil {
-		return err
-	}
-
-	if ok != 1 {
-		return LockErr
-	}
-
-	return nil
 }
